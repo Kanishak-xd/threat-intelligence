793457756bebaac0f5f37a7810cba1feb6ee44e9
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-03-11 23:19:21 +0530

    fixed extra errors

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT;
