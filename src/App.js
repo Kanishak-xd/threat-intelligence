36bba163e3feaaf7202d95e38ce9ed8461db78b7
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-04-07 19:15:36 +0530

    deleted components and cleaned App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from "react";
-import FetchCowrieData from "./components/CowrieFetch";
+import React, { useState } from "react";
 
 function App() {
   return (
     <div>
       <h1>Threat Intelligence Dashboard</h1>
-      <h2>Fetch Cowrie Data from MongoDB</h2>
       <p>Click the button to fetch data from the database.</p>
-      <button onClick={FetchCowrieData}>Fetch Cowrie Data</button>
     </div>
   );
 }
--- src/components/CowrieFetch.js
+++
@@ -1,9 +0,0 @@
-export default function FetchCowrieData() {
-  fetch("http://localhost:5000/api/cowrie-data")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("Fetched cowrie data:", data);
-      alert("Cowrie data fetched! Check console.");
-    })
-    .catch((err) => console.error(err));
-}
