856a583a30266148750b5decef9079ff960369e1
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-04-29 02:37:58 +0530

    changed to deployed version

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 const LOCAL_BACKEND_URL = 'http://localhost:5050';
 const DEPLOYED_BACKEND_URL = 'https://threat-intelligence-pkiv.onrender.com';
 
-// Set this to true to use local backend, false to use deployed backend
+// true for local backend, false for deployed backend
 const USE_LOCAL_BACKEND = false;
 
 export const API_BASE_URL = USE_LOCAL_BACKEND ? LOCAL_BACKEND_URL : DEPLOYED_BACKEND_URL;
