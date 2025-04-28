81c76ba27f92b3d19a13052d181389f7db82f791
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-04-28 18:56:26 +0530

    added avatars & devansh on about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './About.css';
 
 const About = () => {
-  const developers = [
+  const [developers, setDevelopers] = useState([
     {
       name: 'Kanishak Sharma',
-      github: 'https://github.com/Kanishak-xd'
+      github: 'https://github.com/Kanishak-xd',
+      avatar: 'https://github.com/Kanishak-xd.png'
     },
     {
       name: 'Karma Tshering Yangden',
-      github: 'https://github.com/ktyangden'
+      github: 'https://github.com/ktyangden',
+      avatar: 'https://github.com/ktyangden.png'
     },
-    {  
-    name: 'Suhana Mishra',
-      github: 'https://github.com/suhaxa4'
+    {
+      name: 'Suhana Mishra',
+      github: 'https://github.com/suhaxa4',
+      avatar: 'https://github.com/suhaxa4.png'
+    },
+    {
+      name: 'Devansh Arora',
+      github: 'https://github.com/devansharora2812',
+      avatar: 'https://github.com/devansharora2812.png'
     }
-  ];
+  ]);
 
   return (
     <div className="about-container">
@@ -40,6 +48,15 @@ const About = () => {
         <div className="developers-grid">
           {developers.map((developer, index) => (
             <div key={index} className="developer-card">
+              <img 
+                src={developer.avatar} 
+                alt={`${developer.name}'s profile`}
+                className="developer-avatar"
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src = 'https://via.placeholder.com/150?text=No+Image';
+                }}
+              />
               <h3>{developer.name}</h3>
               <a 
                 href={developer.github} 
